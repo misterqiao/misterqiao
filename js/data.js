27ce5485566d128500059030e20cd09563190526
benpaoyinfu <1846139148@qq.com>
2019-09-05 13:46:49 +0800

    Js实战

---
+++ js/data.js
@@ -0,0 +1,31 @@
+
+let data = [
+    {
+        title : "Js"
+        ,author : "奔跑音符"
+        ,time : "2018-07-05"
+        ,topic : "阴阳师官网"
+        ,dec : "这是仿照阴阳师网站做出的前端页面。"
+        ,src : "demo/yys.163.com/index.html"
+        ,img : "demo/yys.163.com/index.png"
+    }
+    ,{
+        title : "Web"
+        ,author : "奔跑音符"
+        ,time : "2018-09-10"
+        ,topic : "兰途科技官网"
+        ,dec : "这是一个响应式的页面"
+        ,src : "demo/lantunet.com/index.html"
+        ,img : "demo/lantunet.com/index.png"
+    }
+    ,{
+        title : "H5"
+        ,author : "奔跑音符"
+        ,time : "2018-09-10"
+        ,topic : "百度我写的"
+        ,dec : "参与了百度首尔有的开发"
+        ,src : "http://www.baidu.com"
+        ,img : "demo/lantunet.com/index.png"
+    }
+    //……
+];
